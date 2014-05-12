137f4ffa9a29c2c9fb05f75161c03efd9e7c3b51
zxsoft <zxsoft@gmail.com>
2014-05-12 23:39:38 +0800

    为百度UEditor的directive添加ng-model双向绑定

--- app/scripts/directives/thirdparty/baiduueditor.js
+++ app/scripts/directives/thirdparty/baiduueditor.js
@@ -8,11 +8,33 @@ angular.module('ngShowcaseApp')
             transclude:true,
             replace:true,
             template:'<script name="content" type="text/plain" ng-transclude>GGG</script>',
+            require:'?ngModel',
             scope:{
                 config:'='
             },
-            link:function(scope,element){
-                new UE.ui.Editor(scope.config || {}).render(element[0]);
+            link:function(scope,element,attrs,ngModel){
+                var editor=new UE.ui.Editor(scope.config || {});
+                editor.render(element[0]);
+
+                if(ngModel) {
+                    //Model数据更新时，更新百度UEditor
+                    ngModel.$render = function(){
+                        try{
+                            editor.setContent(ngModel.$viewValue);
+                        }catch(e){
+
+                        }
+                    }
+
+                    //百度UEditor数据更新时，更新Model
+                    editor.addListener('contentChange',function(){
+                        setTimeout(function () {
+                            scope.$apply(function(){
+                                ngModel.$setViewValue(editor.getContent());
+                            })
+                        }, 0);
+                    })
+                }
             }
         }
     })
