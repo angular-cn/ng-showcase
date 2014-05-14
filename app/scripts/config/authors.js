3afb7ce965f6aeb4ada7097285c7d511d446a65e
zxsoft <zxsoft@gmail.com>
2014-05-14 12:27:59 +0800

    添加zxsoft作者信息

--- app/scripts/config/authors.js
+++ app/scripts/config/authors.js
@@ -6,5 +6,10 @@ angular.module('ngShowcaseApp').constant('authors', [
     name: '雪狼',
     email: 'asnowwolf@gmail.com',
     github: 'https://github.com/asnowwolf'
-  }
+  },{
+        name: 'zxsoft',
+        qq: '21557523',
+        email: 'zxsoft@gmail.com',
+        github: 'https://github.com/zxsoft'
+    }
 ]);
