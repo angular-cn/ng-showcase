9790c3a57359dfe082680afa99dfc6e4d6769449
why520crazy <why520crazy@163.com>
2014-06-10 14:53:01 +0800

    表单基本验证

--- app/scripts/controllers/form/basic.js
+++ app/scripts/controllers/form/basic.js
@@ -1,5 +1,21 @@
 'use strict';
 
 angular.module('ngShowcaseApp').controller('ctrl.form.basic', function ($scope) {
-  var vm = $scope.vm = {};
+    var vm = $scope.vm = {
+        show_error: true,
+        show_type: 1
+    };
+
+    vm.js_submit = function () {
+        vm.show_error = true;
+    };
+
+    vm.js_change_show_type = function (e) {
+        if (vm.show_type == 3) {
+            vm.show_error = false;
+        } else {
+            vm.show_error = true;
+        }
+
+    }
 });
