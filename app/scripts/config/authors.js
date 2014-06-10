b61c572ada6f8363eeb9be661da65ca976027c86
why520crazy <why520crazy@163.com>
2014-06-10 15:05:03 +0800

    表单基本验证

--- app/scripts/config/authors.js
+++ app/scripts/config/authors.js
@@ -2,28 +2,33 @@
 
 // 作者列表
 angular.module('ngShowcaseApp').constant('authors', [
-  {
-    name: 'ngShowcase',
-    github: 'https://github.com/angular-cn/ng-showcase'
-  },
-  {
-    name: '雪狼',
-    email: 'asnowwolf@gmail.com',
-    github: 'https://github.com/asnowwolf'
-  },
-  {
-    name: 'zxsoft',
-    qq: '21557523',
-    email: 'zxsoft@gmail.com',
-    github: 'https://github.com/zxsoft'
-  },
-  {
-    name: 'Ken',
-    github: 'https://github.com/ckken'
-  },
-  {
-    name: 'playing',
-    email: 'bestplayingcn@gmail.com',
-    github: 'https://github.com/playing'
-  }
+    {
+        name: 'ngShowcase',
+        github: 'https://github.com/angular-cn/ng-showcase'
+    },
+    {
+        name: '雪狼',
+        email: 'asnowwolf@gmail.com',
+        github: 'https://github.com/asnowwolf'
+    },
+    {
+        name: 'zxsoft',
+        qq: '21557523',
+        email: 'zxsoft@gmail.com',
+        github: 'https://github.com/zxsoft'
+    },
+    {
+        name: 'Ken',
+        github: 'https://github.com/ckken'
+    },
+    {
+        name: 'playing',
+        email: 'bestplayingcn@gmail.com',
+        github: 'https://github.com/playing'
+    },
+    {
+        name: 'why520crazy',
+        email: 'why520crazy@163.com',
+        github: 'https://github.com/why520crazy'
+    }
 ]);
