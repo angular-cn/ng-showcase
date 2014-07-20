5e12b8c8df0ee7fec66571cd062ac1294ca47d3e
why520crazy <why520crazy@163.com>
2014-07-20 20:47:54 +0800

    验证

--- app/scripts/controllers/form/remoteValidation.js
+++ app/scripts/controllers/form/remoteValidation.js
@@ -1,5 +1,31 @@
 'use strict';
 
-angular.module('ngShowcaseApp').controller('ctrl.form.remoteValidation', function ($scope) {
-  var vm = $scope.vm = {};
-});
+angular.module('ngShowcaseApp')
+    .controller('ctrl.form.remoteValidation', function ($scope) {
+        var vm = $scope.vm = {
+            show_error: false,
+            show_type: 1,
+            user: {}
+        };
+
+        vm.submit = function (basic_form) {
+            vm.show_error = true;
+            basic_form.$setDirty();
+            if (basic_form.$valid) {
+                alert("提交成功！");
+            }
+        };
+
+        vm.change_show_type = function (form) {
+            if (vm.show_type == 2) {
+                vm.show_error = true;
+            } else {
+                vm.show_error = false;
+            }
+
+            // 重置表单
+            vm.user = {};
+            form.$setPristine();
+
+        }
+    });
