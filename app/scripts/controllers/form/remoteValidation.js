e941807f205dc9ba5f8d7950190c24f90e5ec0b7
why520crazy <why520crazy@163.com>
2014-07-22 23:08:55 +0800

    remote validation

--- app/scripts/controllers/form/remoteValidation.js
+++ app/scripts/controllers/form/remoteValidation.js
@@ -28,4 +28,54 @@ angular.module('ngShowcaseApp')
             form.$setPristine();
 
         }
-    });
+    })
+    .directive("remoteUniqueCheck", ['$timeout', '$http',
+        function ($timeout, $http) {
+            return {
+                require: "ngModel",
+                link   : function (scope, elem, attrs, ngModel) {
+
+                    //ngModel.$setValidity('romoteuniquecheck', false);
+                    var doValidate = function () {
+                        var attValues = scope.$eval(attrs.remoteUniqueCheck);
+                        var url = attValues.url;
+                        var isExists = attValues.isExists; //default is true
+                        $http.get(url).success(function (result, status, headers, config) {
+                            if (status === 200) {
+                                if (isExists === false) {
+                                    ngModel.$setValidity('romoteuniquecheck', result.data);
+                                } else {
+                                    ngModel.$setValidity('romoteuniquecheck', !result.data);
+                                }
+
+                            } else {
+                                ngModel.$setValidity('romoteuniquecheck', false);
+                            }
+
+                        });
+                    };
+
+                    scope.$watch(attrs.ngModel, function (newValue) {
+                        if (_.isEmpty(newValue)) {
+                        } else if (!scope[elem[0].form.name][elem[0].name].$dirty) {
+                            doValidate();
+                        }
+                    });
+
+                    elem.bind("blur", function () {
+                        $timeout(function () {
+                            if (scope[elem[0].form.name][elem[0].name].$invalid) {
+                                return;
+                            }
+                            doValidate();
+                        });
+                    });
+                    elem.bind("focus", function () {
+                        $timeout(function () {
+                            ngModel.$setValidity('romoteuniquecheck', true);
+                        });
+                    });
+                }
+            };
+        }
+    ]);
